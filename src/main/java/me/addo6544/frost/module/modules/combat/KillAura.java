fde40c487f0ecbc6488028adca30b89098711de5
Addo6544 <addosama@icloud.com>
2024-02-01 21:09:00 +0800

    Improve KillAura

--- src/main/java/me/addo6544/frost/module/modules/combat/KillAura.java
+++ src/main/java/me/addo6544/frost/module/modules/combat/KillAura.java
@@ -9,6 +9,8 @@ import me.addo6544.frost.module.modules.render.TargetHUDMod;
 import me.addo6544.frost.module.setting.SettingGroup;
 import me.addo6544.frost.module.setting.settings.BooleanSetting;
 import me.addo6544.frost.module.setting.settings.DoubleSetting;
+import me.addo6544.frost.module.setting.settings.IntegerSetting;
+import me.addo6544.frost.module.setting.settings.ModeSetting;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -20,6 +22,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class KillAura extends Module {
+
+    public ModeSetting mode = new ModeSetting("Mode", "", "Single", Arrays.asList(
+            "Single", "Switch", "Multi"
+    ));
+    public ModeSetting sortMode = new ModeSetting("Sort Mode", "", "Random", Arrays.asList(
+            "Random", "Distance"
+    ));
+
     public SettingGroup targetsOption;
     public BooleanSetting targetPlayers = new BooleanSetting("Players", "Target Players", true);
     public BooleanSetting targetAnimals = new BooleanSetting("Animals", "Target Animals", true);
@@ -28,6 +38,7 @@ public class KillAura extends Module {
 
     public SettingGroup attackOption;
     public BooleanSetting noSwing = new BooleanSetting("No Swing", "Don't swing item", false);
+    public IntegerSetting switchDelay = new IntegerSetting("Switch Delay", "", 50, 1000, 300);
 
     public SettingGroup rangeOption;
     public DoubleSetting minAttackRange = new DoubleSetting("Min Attack Range", "Minimum attack range", 0D,6D,0D);
@@ -44,17 +55,21 @@ public class KillAura extends Module {
         tghud = (TargetHUDMod) Frost.INSTANCE.moduleManager.getModule(TargetHUDMod.class);
 
         swingRange.addParent(!noSwing.getConfigValue());
+        sortMode.addParent(!mode.getConfigValue().equals("Multi"));
+        switchDelay.addParent(mode.getConfigValue().equals("Switch"));
 
         targetsOption = new SettingGroup("Targets", Arrays.asList(
                 targetPlayers,targetAnimals,targetMobs,targetInvisible
         ));
         attackOption = new SettingGroup("Attack Options", Arrays.asList(
-                noSwing
+                noSwing,switchDelay
         ));
         rangeOption = new SettingGroup("Range Options", Arrays.asList(
                 minAttackRange,maxAttackRange,swingRange
         ));
 
+        this.settings.addSetting(mode);
+        this.settings.addSetting(sortMode);
         this.settings.addSetting(targetsOption);
         this.settings.addSetting(attackOption);
         this.settings.addSetting(rangeOption);
@@ -63,6 +78,7 @@ public class KillAura extends Module {
     @EventTarget
     public void onUpdate(EventUpdate e){
         processEntities(mc.theWorld.loadedEntityList);
+        if (targets.isEmpty()) return;
         attack();
     }
 
@@ -71,7 +87,7 @@ public class KillAura extends Module {
         for (Entity e : entities){
             if (e.isDead) continue;
 
-            float d = e.getDistanceToEntity(mc.thePlayer);
+            float d = mc.thePlayer.getDistanceToEntity(e);
 
             if (d > maxAttackRange.getConfigValue() && d > swingRange.getConfigValue()) continue;
             if (d < minAttackRange.getConfigValue()) continue;
@@ -83,22 +99,48 @@ public class KillAura extends Module {
 
         tgTmp.removeIf(e -> e.isInvisible() && !targetInvisible.getConfigValue());
 
+        /***
         for (EntityLivingBase e : targets){
             if (!tgTmp.contains(e)){
                 targets.remove(e);
                 if (e instanceof AbstractClientPlayer) tghud.getHud().setTarget(null);
             }
         }
+         ***/
+        targets.clear();
+        tghud.getHud().setTarget(null);
 
         targets.addAll(tgTmp);
     }
 
     private void attack(){
-        for (EntityLivingBase tg : targets){
+        if (mode.getConfigValue().equalsIgnoreCase("Multi")) {
+            for (EntityLivingBase tg : targets) {
+                if (tg instanceof AbstractClientPlayer)
+                    tghud.getHud().setTarget((AbstractClientPlayer) tg);
+                if (mc.thePlayer.getDistanceToEntity(tg) <= swingRange.getConfigValue() && !noSwing.getConfigValue())
+                    mc.thePlayer.swingItem();
+                if (mc.thePlayer.getDistanceToEntity(tg) <= maxAttackRange.getConfigValue()) {
+                    mc.playerController.attackEntity(mc.thePlayer, tg);
+                    if (!noSwing.getConfigValue()) mc.thePlayer.swingItem();
+                }
+            }
+        } else if (mode.getConfigValue().equalsIgnoreCase("Single")){
+            EntityLivingBase tg = null;
+
+            if (sortMode.getConfigValue().equalsIgnoreCase("Random"))
+                emptyMethod();
+            else if (sortMode.getConfigValue().equalsIgnoreCase("Distance"))
+                targets.sort((o1, o2) -> (int) ((mc.thePlayer.getDistanceToEntity(o1)) - (mc.thePlayer.getDistanceToEntity(o2))));
+
+            tg=targets.get(0);
+
+
             if (tg instanceof AbstractClientPlayer)
                 tghud.getHud().setTarget((AbstractClientPlayer) tg);
-            if (tg.getDistanceToEntity(mc.thePlayer) <= swingRange.getConfigValue() && !noSwing.getConfigValue()) mc.thePlayer.swingItem();
-            if (tg.getDistanceToEntity(mc.thePlayer) <= maxAttackRange.getConfigValue()){
+            if (mc.thePlayer.getDistanceToEntity(tg) <= swingRange.getConfigValue() && !noSwing.getConfigValue())
+                mc.thePlayer.swingItem();
+            if (mc.thePlayer.getDistanceToEntity(tg) <= maxAttackRange.getConfigValue()) {
                 mc.playerController.attackEntity(mc.thePlayer, tg);
                 if (!noSwing.getConfigValue()) mc.thePlayer.swingItem();
             }
@@ -118,5 +160,9 @@ public class KillAura extends Module {
         targets.clear();
         tghud.getHud().setVisible(false);
         tghud.getHud().setTarget(null);
+    }
+
+    private void emptyMethod(){
+
     }
 }
